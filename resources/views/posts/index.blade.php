afb99e6ab92a3c93c7fb389a0000a465756b6ac0
ccestil <c.estil.543843@umindanao.edu.ph>
2025-04-19 18:26:46 +0800

    added some design

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,29 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
 </head>
+
+<style>
+
+    .container {
+        max-width: 1280px;
+        margin: 0px auto;
+    }
+
+    li {
+        list-style: none;
+        background-color: rgb(240, 240, 240);
+        padding: 30px;
+        margin-bottom: 30px
+    }
+</style>
+
 <body>
-    <nav>
-        <a href="{{ route('posts.create') }}">Write a blog</a> </nav>
 
-    @forelse ($posts as $post )
+    <div class="container">
 
-    <li>
-        <h1>{{ $post->title}}</h1>
-        <p>
-            {{ $post->body }}
-
-        </p>
-    </li>
-        
-    @empty
-    <p>There is no blogs available.</p>
-    @endforelse
+        <nav>
+            <a href="{{ route('posts.create') }}">Write a blog</a>
+        </nav>
     
+        @forelse ($posts as $post)
+            <li>
+                <h1>{{ $post->title }}</h1>
+                <p>
+                    {{ $post->body }}
+    
+                </p>
+            </li>
+    
+        @empty
+            <p>There is no blogs available.</p>
+        @endforelse
+
+    </div>
+
 
 </body>
+
 </html>
